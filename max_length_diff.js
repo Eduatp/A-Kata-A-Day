3495b932dedc386ada1fdb67e4840e267427bb51
Varun Upadhyay <varun.u28@gmail.com>
2015-12-19 05:44:51 +0530

    Day 23

--- max_length_diff.js
+++ max_length_diff.js
@@ -1,3 +1,19 @@
+//Maximum length difference
+//Level:7kyu
+/*Problem Description: You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to
+z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 or a2 are empty return -1 in each language except in Haskell where you will return Nothing.
+
+Example:
+
+s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(s1, s2) --> 13
+*/
+
 function mxdiflg(a1, a2) {
     if(a1.length===0 || a2.length===0)
     {
