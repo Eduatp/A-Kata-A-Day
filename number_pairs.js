8234ab7a53aa53ff240666cbc80f9a9c8f752182
Varun Upadhyay <varun.u28@gmail.com>
2015-12-24 19:18:18 +0530

    Day 28

--- number_pairs.js
+++ number_pairs.js
@@ -1,3 +1,18 @@
+//Number pairs
+//Level: 7kyu
+/*
+Problem Description: In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array 
+of large numbers.
+
+Note: Both arrays have the same dimensions.
+
+Example:
+
+let arr1 = [13, 64, 15, 17, 88];
+let arr2 = [23, 14, 53, 17, 80];
+getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
+*/
+
 function getLargerNumbers(a, b) {
   var ans=[];
   var i=0;
