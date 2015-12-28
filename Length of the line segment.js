870e4f50b34808b42289356c94fba637c21df098
Varun Upadhyay <varun.u28@gmail.com>
2015-12-28 07:23:12 +0530

    Day 32

--- Length of the line segment.js
+++ Length of the line segment.js
@@ -1,3 +1,17 @@
+//Length of the line segment
+//Level: 7kyu
+/*
+Problem Description: Find the length between 2 co-ordinates. The co-ordinates are made of integers between -20 and 20 
+and will be given in the form of a 2D array:
+
+(0,0) and (5,-7) would be [ [ 0 , 0 ] , [ 5, -7 ] ]
+
+The function must return the answer rounded to 2 decimal places in the form of a string.
+
+lengthOfLine([ [ 0 , 0 ] , [ 5, -7 ] ]); => "8.60"
+If the 2 given co-ordinates are the same, the returned length should be "0.00"
+*/
+
 function lengthOfLine(array){
   var x1=array[0][0];
   var y1=array[0][1];
