a9663305fbe4ff18a6a63b6e9d86fd606f488d58
Varun Upadhyay <varun.u28@gmail.com>
2015-12-30 07:55:06 +0530

    Day 34

--- 80's Kids #3 Punky Brewster's Socks.js
+++ 80's Kids #3 Punky Brewster's Socks.js
@@ -1,3 +1,19 @@
+//80's Kids #3: Punky Brewster's Socks
+//Level:7kyu
+/*
+Problem Description: Punky loves wearing different colored socks, but Henry can't stand it.
+
+Given an array of different colored socks, return a pair depending on who was picking them out.
+
+Example:
+
+getSocks('Punky',['red','blue','blue','green']) -> ['red', 'blue']
+Note that punky can have any two colored socks, in any order, as long as they are different and both exist. Henry 
+always picks a matching pair.
+
+If there is no possible combination of socks, return an empty array.
+*/
+
 function getSocks(name, socks) 
 {
     socks.sort();
