716e6c7ac683d1e1c30f61185964a14c628929b2
Varun Upadhyay <varun.u28@gmail.com>
2016-01-02 14:17:26 +0530

    Day 37

--- Split In Parts.js
+++ Split In Parts.js
@@ -1,3 +1,24 @@
+//Split in Parts
+//Level:7kyu
+/*
+Problem Description: The aim of this kata is to split a given string into different strings of equal size (note size
+ of strings is passed to the method)
+
+Example:
+
+Split the below string into other strings of size #3
+
+'supercalifragilisticexpialidocious'
+
+Will return a new string
+'sup erc ali fra gil ist ice xpi ali doc iou s'
+Assumptions:
+
+String length is always greater than 0
+String has no spaces
+Size is always positive
+*/
+
 var splitInParts = function(s, partLength)
 {
     var ans="";
