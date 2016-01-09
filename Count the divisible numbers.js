5aba2cdfb6624219d4278476ed38a7a1b4ba5631
Varun Upadhyay <varun.u28@gmail.com>
2016-01-09 07:40:12 +0530

    Day 44

---
+++ Count the divisible numbers.js
@@ -0,0 +1,25 @@
+//Count the divisible numbers
+//Level:6kyu
+/*
+Problem Description: Write function divisibleCount(x, y, k) that takes in 3 numbers x, y and k, and returns the number
+of integers within the range [x..y] that are divisible by k i.e.: 
+{ i : x ≤ i ≤ y, i mod k = 0 }
+For example:
+x = 6, y = 11 and k = 2, your function should return 3, because there are three numbers divisible by 2 within the 
+range [6..11], namely 6, 8 and 10.
+Note: x<=y
+*/
+
+function divisibleCount(x, y, k) {
+   var x_mod = x % k;
+
+    if(x_mod !== 0)
+        x += (k - x_mod);
+    
+    y -= y % k;
+    
+    if(x > y)
+        return 0;
+    else
+        return (y - x) / k + 1;
+}
