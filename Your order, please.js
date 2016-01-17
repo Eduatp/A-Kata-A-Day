6bc89ecd9a901ccf8e1865170c632bac89f18a4e
Varun Upadhyay <varun.u28@gmail.com>
2016-01-17 08:52:28 +0530

    Day 52

---
+++ Your order, please.js
@@ -0,0 +1,28 @@
+//Your order, please
+//Level:6kyu
+/*
+Problem Description: Your task is to sort a given string. Each word in the String will contain a single number. This 
+number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input String is empty, return an empty String. The words in the input String will only contain valid 
+consecutive numbers.
+
+For an input: "is2 Thi1s T4est 3a" the function should return "Thi1s is2 3a T4est"
+*/
+
+function order(words){
+    var arr=words.split(" ");
+    var ans=[];
+    
+    var i=0;
+    while(i<arr.length)
+    {
+        var thenum = parseInt(arr[i].replace( /^\D+/g, ''));
+        ans[thenum-1]=arr[i];
+        i++;
+    }
+    //var thenum = thestring.replace( /^\D+/g, '');
+    return ans.join(" ");
+}
