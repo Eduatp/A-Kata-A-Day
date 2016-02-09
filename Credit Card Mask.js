e1896903cdff36f645adf310337827b9afccc7b2
Varun Upadhyay <varun.u28@gmail.com>
2016-02-09 20:30:32 +0530

    Day 70

---
+++ Credit Card Mask.js
@@ -0,0 +1,35 @@
+//Credit Card Mask
+//Level: 7kyu
+/*
+Problem Description: Usually when you buy something, you're asked whether your credit card number, phone number or 
+answer to your most secret question is still correct. However, since someone could look over your shoulder, you 
+don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples
+
+maskify("4556364607935616") == "############5616"
+maskify(     "64607935616") ==      "#######5616"
+maskify(               "1") ==                "1"
+maskify(                "") ==                 ""
+
+// "What was the name of your first pet?"
+maskify("Skippy")                                   == "##ippy"
+maskify("Nananananananananananananananana Batman!") == "######################################man!"
+*/
+
+function maskify(cc)
+{
+    if(cc.length<=4)
+    {
+        return cc;
+    }
+    var i=0;
+    while(i<cc.length-4)
+    {
+        cc=cc.replace(cc[i],'#');
+        i++;
+    }
+    return cc;
+}
